ff608e8a9f840dccb59cd74b386cadb76e03276e
Luke Baker <lukebaker@gmail.com>
2007-03-01 15:36:40 +0000

    added initial Json support for defining associations

--- models/Post.php
+++ models/Post.php
@@ -2,7 +2,7 @@
 require_once 'activerecord' .DIRECTORY_SEPARATOR. 'generated_models' .DIRECTORY_SEPARATOR. 'PostBase.php';
 class Post extends PostBase {
 
-  protected $has_many = array(array('comments' => array('dependent' => 'destroy')), 'categorizations',
+  protected $has_many = array('{"comments" : {"dependent" : "destroy"}}', 'categorizations',
     array('categories' => array('through' => 'categorizations')));
   protected $has_one  = array('slug');
 }
