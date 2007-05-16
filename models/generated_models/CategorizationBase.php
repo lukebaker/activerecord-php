5ffd79176ced46ed766adff19a6bb342d616bb9e
Luke Baker <lukebaker@gmail.com>
2007-05-16 18:37:40 +0000

    move generated models to models dir

---
+++ models/generated_models/CategorizationBase.php
@@ -0,0 +1,14 @@
+<?php
+require_once dirname(__FILE__) .DIRECTORY_SEPARATOR. '..' .DIRECTORY_SEPARATOR. 'ActiveRecord.php';
+
+class CategorizationBase extends ActiveRecord {
+
+  protected $columns = array('id', 'post_id', 'category_id');
+  protected $table_name = 'categorizations';
+  protected $primary_key = 'id';
+
+  static function find($id, $options=null) {
+    return parent::find(__CLASS__, $id, $options);
+  }
+}
+?>
---
+++ models/generated_models/SlugBase.php
@@ -0,0 +1,14 @@
+<?php
+require_once dirname(__FILE__) .DIRECTORY_SEPARATOR. '..' .DIRECTORY_SEPARATOR. 'ActiveRecord.php';
+
+class SlugBase extends ActiveRecord {
+
+  protected $columns = array('id', 'slug', 'post_id');
+  protected $table_name = 'slugs';
+  protected $primary_key = 'id';
+
+  static function find($id, $options=null) {
+    return parent::find(__CLASS__, $id, $options);
+  }
+}
+?>
